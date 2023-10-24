8a826a8585d53a0398318e4fbfe7851b44dddbcc
Yeji Ha <yeji206000@gmail.com>
2023-10-24 00:45:33 -0700

    function added

--- eqArrays.js
+++ eqArrays.js
@@ -5,3 +5,20 @@ const assertEqual = function(actual, expected) {
     console.log(`🛑🛑🛑 Assertion Failed: ${actual} !== ${expected}`);
   }
 };
+
+const eqArrays = function(arr1, arr2) {
+  for (let i = 0; i < arr1.length; i++) {
+    if (arr1[i] !== arr2[i]) {
+      return false;
+    }
+  }
+  return true;
+};
+
+console.log(eqArrays([1, 2, 3], [1, 2, 3]));
+console.log(eqArrays([1, 2, 3], [3, 2, 1]));
+
+console.log(eqArrays(["1", "2", "3"], ["1", "2", "3"]));
+console.log(eqArrays(["1", "2", "3"], ["1", "2", 3]));
+
+console.log(assertEqual(eqArrays([1, 2, 3], [1, 2, 3]), true));
