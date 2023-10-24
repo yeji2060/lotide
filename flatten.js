4e67ec92addf6143f7a75e37475189b567ed10b1
Yeji Ha <yeji206000@gmail.com>
2023-10-24 01:39:24 -0700

    function added

--- flatten.js
+++ flatten.js
@@ -19,16 +19,16 @@ const eqArrays = function(arr1, arr2) {
   return true;
 };
 
-const without = function(source, itemsToRemove) {
-  let removed = [];
-  for (let i = 0; i < source.length; i++) {
-    if (!itemsToRemove.includes(source[i])) {
-      removed.push(source[i]);
+const flatten = function(arr) {
+  var result = [];
+
+  for (var i = 0; i < arr.length; i++) {
+    if (Array.isArray(arr[i])) {
+      result = result.concat(arr[i]);
+    } else {
+      result.push(arr[i]);
     }
   }
-  return removed;
-}
 
-const words = ["hello", "world", "lighthouse"];
-assertArraysEqual(without(words, ["lighthouse"]), ["hello", "world"]);
-assertArraysEqual(words, ["hello", "world", "lighthouse"]);
+  return result;
+}
