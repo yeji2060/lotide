698193d1b2c340dafcf47424ac74fe2a058a29e0
Yeji Ha <yeji206000@gmail.com>
2023-10-30 22:30:21 -0700

    edited

--- letterPositions.js
+++ letterPositions.js
@@ -1,3 +1,24 @@
+const assertArraysEqual = function(arr1, arr2) {
+  if (eqArrays(arr1,arr2) == true) {
+    console.log(`✅✅✅ Assertion Passed: ${arr1} === ${arr2}`);
+  } else {
+    console.log(`🛑🛑🛑 Assertion Failed: ${arr1} !== ${arr2}`);
+  }
+}
+
+const eqArrays = function(arr1, arr2) {
+  if (arr1.length !== arr2.length) {
+    return false;
+  }
+  
+  for (let i = 0; i < arr1.length; i++) {
+    if (arr1[i] !== arr2[i]) {
+      return false;
+    }
+  }
+  return true;
+};
+
 const letterPositions = function(str) {
   const result = {};
   for (let i = 0; i < str.length; i++) {
