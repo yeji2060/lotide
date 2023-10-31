bfc830ce32f551e9d2783df5684d4fab54fcccf6
Yeji Ha <yeji206000@gmail.com>
2023-10-31 02:08:39 -0700

    function added

---
+++ eqObjects.js
@@ -0,0 +1,49 @@
+const eqArrays = function(arr1, arr2) {
+  if (arr1.length !== arr2.length) {
+    return false;
+  }
+  
+  for (let i = 0; i < arr1.length; i++) {
+    if (arr1[i] !== arr2[i]) {
+      return false;
+    }
+  }
+  return true;
+};
+
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log(`✅✅✅ Assertion Passed: ${actual} === ${expected}`);
+  } else {
+    console.log(`🛑🛑🛑 Assertion Failed: ${actual} !== ${expected}`);
+  }
+};
+
+const eqObjects = function(object1, object2) {
+
+
+
+  if (Object.keys(object1).length == Object.keys(object2).length) {
+    for(keys in Object.keys(object1)) {
+      if(object1[keys] !== object2[keys]) {
+        console.log("Here");
+        return false;
+      }
+      if(Array.isArray(object1[keys])) {
+        if (eqArrays(object1[keys], object2[keys]) == false) {
+          return false;
+        }
+      }
+    }
+    return true;
+  }
+  return false;
+}
+
+
+const multiColorShirtObject = { colors: ["red", "blue"], size: "medium" };
+const anotherMultiColorShirtObject = { size: "medium", colors: ["red", "blue"] };
+console.log(eqObjects(multiColorShirtObject  , anotherMultiColorShirtObject)); // => true
+
+const longSleeveMultiColorShirtObject= { size: "medium", colors: ["red", "blue"], sleeveLength: "long" };
+console.log(eqObjects(multiColorShirtObject  , longSleeveMultiColorShirtObject)); // => false
