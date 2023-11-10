52ad59381febcc84dc5ba011dc9a2d6afd9e3d32
Yeji Ha <yeji206000@gmail.com>
2023-11-09 19:37:31 -0800

    edited

--- takeUntil.js
+++ takeUntil.js
@@ -22,9 +22,9 @@ const eqArrays = function(arr1, arr2) {
 const takeUntil = function(array, callback) {
   const result = [];
   for (let item of array) {
-    if (callback(item) === false) {
+    if (!callback(item)) {
       result.push(item);
-    } else if (callback(item) == true) {
+    } else if (callback(item) ) {
       return result;
     }
   }
@@ -33,12 +33,12 @@ const takeUntil = function(array, callback) {
 
 const data1 = [1, 2, 5, 7, 2, -1, 2, 4, 5];
 const results1 = takeUntil(data1, x => x < 0);
-console.log(assertArraysEqual(results1,[ 1, 2, 5, 7, 2 ]));
+assertArraysEqual(results1,[ 1, 2, 5, 7, 2 ]);
 
 console.log('---');
 
 const data2 = ["I've", "been", "to", "Hollywood", ",", "I've", "been", "to", "Redwood"];
 const results2 = takeUntil(data2, x => x === ',');
-console.log(assertArraysEqual(results2, [ "I've", 'been', 'to', 'Hollywood' ]));
+assertArraysEqual(results2, [ "I've", 'been', 'to', 'Hollywood' ]);
 
 
