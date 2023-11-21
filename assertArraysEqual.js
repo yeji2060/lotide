62e961e55c62dec4a4fa7e7dac32662133ee8bb0
Yeji Ha <yeji206000@gmail.com>
2023-11-21 12:00:51 -0800

    refractor

--- assertArraysEqual.js
+++ assertArraysEqual.js
@@ -1,25 +1,11 @@
+const eqArrays = require('./eqArrays');
+
 const assertArraysEqual = function(arr1, arr2) {
-  if (eqArrays(arr1,arr2) == true) {
+  if (eqArrays(arr1, arr2) == true) {
     console.log(`✅✅✅ Assertion Passed: ${arr1} === ${arr2}`);
   } else {
     console.log(`🛑🛑🛑 Assertion Failed: ${arr1} !== ${arr2}`);
   }
 }
 
-const eqArrays = function(arr1, arr2) {
-  if (arr1.length !== arr2.length) {
-    return false;
-  }
-  
-  for (let i = 0; i < arr1.length; i++) {
-    if (arr1[i] !== arr2[i]) {
-      return false;
-    }
-  }
-  return true;
-};
-
-const args = process.argv;
-let arr = args.slice(2);
-
-assertArraysEqual(arr[0],arr[1]);
+module.exports = assertArraysEqual;
---
+++ test/assertArraysEqualTest.js
@@ -0,0 +1,4 @@
+const assertArraysEqual = require('../assertArraysEqual');
+
+assertArraysEqual([1,2,3], [1,2,3]); // should pass
+assertArraysEqual([1,2,3], [1,2]); // should fail
