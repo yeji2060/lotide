fe145918baab895851188edaec5ab2091330c871
Yeji Ha <yeji206000@gmail.com>
2023-11-21 12:09:51 -0800

    refractor middle

--- middle.js
+++ middle.js
@@ -1,24 +1,3 @@
-const assertArraysEqual = function(arr1, arr2) {
-  if (eqArrays(arr1,arr2)) {
-    console.log(`✅✅✅ Assertion Passed: ${arr1} === ${arr2}`);
-  } else {
-    console.log(`🛑🛑🛑 Assertion Failed: ${arr1} !== ${arr2}`);
-  }
-}
-
-const eqArrays = function(arr1, arr2) {
-  if (arr1.length !== arr2.length) {
-    return false;
-  }
-  
-  for (let i = 0; i < arr1.length; i++) {
-    if (arr1[i] !== arr2[i]) {
-      return false;
-    }
-  }
-  return true;
-};
-
 const middle = function(arr) {
   let result = [];
   if (arr.length == 1 || arr.length == 2) {
@@ -37,8 +16,4 @@ const middle = function(arr) {
 
 }
 
-console.log(middle([1, 2, 3]));
-console.log(middle([1, 2, 3, 4, 5]));
-console.log(middle([1, 2, 3, 4]));
-console.log(middle([1, 2, 3, 4, 5, 6]));
-
+module.exports = middle;
