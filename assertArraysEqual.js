99240710c7f87092456938b7454804d7bba5a26b
Yeji Ha <yeji206000@gmail.com>
2023-11-21 12:01:45 -0800

    edited

--- assertArraysEqual.js
+++ assertArraysEqual.js
@@ -1,7 +1,7 @@
 const eqArrays = require('./eqArrays');
 
 const assertArraysEqual = function(arr1, arr2) {
-  if (eqArrays(arr1, arr2) == true) {
+  if (eqArrays(arr1, arr2)) {
     console.log(`✅✅✅ Assertion Passed: ${arr1} === ${arr2}`);
   } else {
     console.log(`🛑🛑🛑 Assertion Failed: ${arr1} !== ${arr2}`);
