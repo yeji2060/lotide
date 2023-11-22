34b351ac590f614cbb43dc0de30a6be3fd039a5e
Yeji Ha <yeji206000@gmail.com>
2023-11-22 09:20:55 -0800

    mocha and chai added

--- test/headTest.js
+++ test/headTest.js
@@ -1,5 +1,20 @@
+const assert = require('chai').assert;
 const head = require('../head');
 const assertEqual = require('../assertEqual');
 
-assertEqual(head([6,6,7]), 6);
-assertEqual(head(["Hello", "Lighthouse", "Labs"]), "Hello");
+describe("#head", () => {
+  it("returns 1 for [1, 2, 3]", () => {
+    assert.strictEqual(head([1, 2, 3]), 1);
+  });
+
+  it("returns 6 for [6, 7, 8]", () => {
+    assert.strictEqual(head([6, 7, 8]), 6)
+  })
+
+  it("returns Hello for [Hello, Lighthouse, Labs]", () => {
+    assert.strictEqual(head(["Hello", "Lighthouse", "Labs"]), "Hello");
+  })
+});
+
+
+
